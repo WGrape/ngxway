6dc945aa767432c8bd6b71e0aea36911957356d9
lvsi <15239928381@163.com>
2023-02-05 23:29:57 +0800

    style: change check_sign.lua code style

--- test/benchmark.sh
+++ test/benchmark.sh
@@ -1,10 +1,11 @@
-# wrk -t 13 -c 1000 -d 30s 'http://127.0.0.1:8090/api/test?sign=2b5f453e8c5db94a12&timestamp=1675582136'
-# ab -n 70000 -c 15 -k "http://127.0.0.1:8090/api/test?sign=2b5f453e8c5db94a12&timestamp=1675582136"
+#!/usr/bin/env bash
 
 currentDir=$(pwd)
 . $currentDir/bin/common.sh
 
-computeSignedAPI
+computeSignedRequest
+
+# test the api
 echo -e ">> Benchmark command : ab -n 50000 -c 15 -k \"${signedAPI}\""
 echo -e
 ab -n 50000 -c 15 -k "${signedAPI}"

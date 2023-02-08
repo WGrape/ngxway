3ee5699592b48075a58d4ba98e98cf68e5f31d3e
lvsi <15239928381@163.com>
2023-02-08 11:11:40 +0800

    fix: fix the bug of stop.sh for docker detecting

--- stop.sh
+++ stop.sh
@@ -3,25 +3,19 @@
 # Include common.sh
 . bin/common.sh
 
-# Stop and remove container
-docker stop ngxwayContainer
+# Detect whether docker is started by commands such as docker images, and redirect the output so that the content will not be output on the console
+docker images > /dev/null 2>&1
 if [ $? -ne 0 ]; then
   echo ">>>>>>>>Stop failure: please start docker first<<<<<<<<"
   exit 1
 fi
 
+# Stop and remove container
+docker stop ngxwayContainer
 docker container rm ngxwayContainer
-if [ $? -ne 0 ]; then
-  echo ">>>>>>>>Stop failure: failed to rm container<<<<<<<<"
-  exit 1
-fi
 
 # Remove image
 docker rmi ngxway
-if [ $? -ne 0 ]; then
-  echo ">>>>>>>>Stop failure: failed to rm image<<<<<<<<"
-  exit 1
-fi
 
 # Remove local volume logs
 rm -rf $local_volume_logs_dir

8388d92d1ab2715ffe2df59a4ec998a2a6aba1af
lvsi <15239928381@163.com>
2023-02-18 22:51:20 +0800

    feat: remove log script files

--- scripts/logs/show_accesslog.sh
+++
@@ -1,9 +0,0 @@
-# Go back to the root directory of ngxway.
-ngxwayPath=$(cd `dirname $0`; cd ../../; pwd)
-cd $ngxwayPath
-
-# Require the common shell scripts.
-. $ngxwayPath/scripts/common.sh
-
-cd $localVolumeLogsDir
-tail -f ngxway.access.log
